cc5bd174f3c0963bab3940a4d3e0a7c3baa1037c
2210312801@qq.com <2210312801@qq.com>
2022-12-08 22:32:39 +0800

    feat: add card components

---
+++ src/base-ui/card/src/card.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>{{ title }}</span>
+      </div>
+    </template>
+    <div class="item">
+      <slot></slot>
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: "Title"
+    }
+  },
+
+  setup(props) {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.item {
+  margin-bottom: 10px;
+}
+</style>
--- src/view/main/analysis/dashboard/dashboard.vue
+++ src/view/main/analysis/dashboard/dashboard.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="dashboard">
-    <h2>dashboard</h2>
+    <el-row :gutter="10">
+      <el-col :span="7">
+        <Card title="分类商品数量(饼图)"></Card>
+      </el-col>
+      <el-col :span="10">
+        <Card title="不同城市商品销量"></Card>
+      </el-col>
+      <el-col :span="7">
+        <Card title="分类商品数量(玫瑰图)"></Card>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="10" class="content-row">
+      <el-col :span="12">
+        <Card title="分类商品的销量"></Card>
+      </el-col>
+      <el-col :span="12">
+        <Card title="分类商品的收藏"></Card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
-
+import { defineComponent, ref, onMounted } from "vue"
+import * as echarts from "echarts"
+import Card from "@/base-ui/card"
 export default defineComponent({
   name: "dashboard",
+  components: {
+    Card
+  },
   setup() {
-    return {}
+    const divRef = ref<HTMLElement>()
+
+    return { divRef }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.content-row {
+  margin-top: 20px;
+}
+</style>
